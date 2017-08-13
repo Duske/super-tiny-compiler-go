f52f3c1f2b33d9ba8c40bec564c60961edac7bf8
Dustin Chabrowski <dustin.duske@gmail.com>
2017-08-13 22:40:48 +0200

    [TASK] add tests for parser

---
+++ parser/parser_test.go
@@ -0,0 +1,45 @@
+package parser
+
+import (
+	"testing"
+	"super-tiny-compiler/tokenizer"
+	"reflect"
+)
+
+func TestParserWithEmptyTokens(t *testing.T) {
+	input := []tokenizer.Token{}
+	correctAST := AST{typeName: "Program"}
+	output := Parse(input)
+	if !reflect.DeepEqual(correctAST, output)  {
+		t.Log("Empty tokens does not create an correct AST")
+		t.Fail()
+	}
+}
+
+func TestParserWithTokens(t *testing.T) {
+	input := []tokenizer.Token{
+		{TypeName: "paren", Value: "("},
+		{TypeName: "name", Value: "add"},
+		{TypeName: "number", Value: "2"},
+		{TypeName: "paren", Value: "("},
+		{TypeName: "name", Value: "subtract"},
+		{TypeName: "number", Value: "4"},
+		{TypeName: "number", Value: "2"},
+		{TypeName: "paren", Value: ")"},
+		{TypeName: "paren", Value: ")"},
+	}
+	correctAST := AST{typeName: "Program", body: []ASTType{
+		CallExpression{value: "add", params: []ASTType{
+			NumberLiteral{ "2"},
+			CallExpression{value: "subtract", params: []ASTType{
+				NumberLiteral{"4"},
+				NumberLiteral{"2"},
+			}},
+		}},
+	}}
+	output := Parse(input)
+	if !reflect.DeepEqual(correctAST, output)  {
+		t.Log("AST is not correct")
+		t.Fail()
+	}
+}
